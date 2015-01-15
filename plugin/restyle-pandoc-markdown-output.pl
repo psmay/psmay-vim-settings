6bb48c8eeb96294266127b6e9bc08a549c3a277e
Peter S. May <psmay@curogens.com>
2015-01-15 13:24:57 -0500

    exec-with-unix-shell and markdown-to-markdown
    * exec-with-unix-shell now guesses and checks where Cygwin might be in any of a few common places.
    * markdown-to-markdown helper scripts are now part of the repo and do not need to be installed in PATH.

---
+++ plugin/restyle-pandoc-markdown-output.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl
+
+# Tacky script for removing some of the hateful "upgrades" applied by pandoc
+# when formatting markdown.
+
+use warnings;
+use strict;
+use 5.010;
+use Carp;
+
+while(<>) {
+	chomp;
+	# Use stars for lists
+	s/^((?:    )*)-(   )/$1*$2/g;
+	# Kill excessive backslashes
+	s/\\([<>&;*#~_])/$1/g;
+	say;
+}
